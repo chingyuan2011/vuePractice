7ebbf21eb4c90fa6bd61ed784d21a3111e8b24d1
ryan.yang <ryan.yang@104.com.tw>
2022-06-04 16:29:05 +0800

    [練習] emits 使用與驗證

---
+++ src/components/practice/Emits.vue
@@ -0,0 +1,29 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+import { defineEmits, ref, onMounted } from 'vue'
+const num = ref(0)
+
+const emits = defineEmits({
+  Callback: (num) =>{
+    if(num.value === 0){
+      return true
+    } else {
+      // 驗證失敗
+      return false
+    }
+  }
+})
+
+onMounted(()=>{
+  emits('Callback', num)
+})
+
+
+</script>
+
+<style>
+
+</style>
--- src/views/Practice.vue
+++ src/views/Practice.vue
@@ -5,11 +5,14 @@
       :multiProps="123"
     >
     </PropsChildren>
+    <EmitsChildren></EmitsChildren>
   </div>
 </template>
 
 <script setup>
 import PropsChildren from "@/components/practice/Props";
+import EmitsChildren from "@/components/practice/Emits";
+
 import { ref } from "vue";
 
 const str = ref('props test')
