8e6500a8a9757e9a0ea39c86a184d5df90c124da
ryan.yang <ryan.yang@104.com.tw>
2022-06-04 16:27:52 +0800

    [練習] props 父子組件設定

---
+++ src/components/practice/Props.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>{{ str }}</div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+// const props = defineProps([ 'propsData' ])
+const props = defineProps({
+  multiProps: {
+    type: [Number, String]
+  },
+  str: {
+    type: String,
+    default: '',
+    required: true,
+  },
+  num: {
+    type: Number,
+    default: 0
+  },
+  arr: {
+    type: Array,
+    default: () => []
+  },
+  obj: {
+    type: Object,
+    default: () => ({})
+  },
+  fun: {
+    type: Function,
+    default: () => {}
+  }
+})
+
+</script>
+
+<style lang="scss" scoped>
+div {
+  background-color: red;
+}
+</style>
---
+++ src/views/Practice.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <PropsChildren
+      :str="str"
+      :multiProps="123"
+    >
+    </PropsChildren>
+  </div>
+</template>
+
+<script setup>
+import PropsChildren from "@/components/practice/Props";
+import { ref } from "vue";
+
+const str = ref('props test')
+
+</script>
+
+<style>
+.Practice {
+  color: red;
+}
+div {
+  color: #fff;
+}
+</style>
